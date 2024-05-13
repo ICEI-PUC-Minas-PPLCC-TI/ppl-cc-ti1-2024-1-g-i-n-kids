3f91899b5637e05a0c12ab337d623b86721ccf0d
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-05-13 13:53:04 -0300

    feat: adição das postagens na tela de gerenciamento

--- codigo/assets/js/postagens/gerenciar-postagens.js
+++ codigo/assets/js/postagens/gerenciar-postagens.js
@@ -1,1 +1,29 @@
-console.log('JavaScript da página de gerenciar postagem');
+var db = [];
+
+readPost(data => {
+    db = data;
+    listPosts();
+});
+
+function listPosts() {
+    let divPostagens = document.getElementById('postagens');
+
+    divPostagens.innerHTML = '';
+
+    for (let i = 0; i < db.length; i++) {
+        const post = db[i];
+
+        divPostagens.innerHTML += `
+                                    <div class="card" style="width: 18rem;">
+                                        <h3 class="card-title">${post.titulo}</h3>
+                                        <img class="card-img-top" src="${post.link_imagem}" alt="Imagem da postagem">
+                                        <div class="card-body">
+                                            <div class="icons">
+                                                <i class="fa-regular fa-rectangle-xmark delete"></i>
+                                                <a href="./editar-postagem.html"><i class="fa-solid fa-pen edit"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    `;
+        }
+}
