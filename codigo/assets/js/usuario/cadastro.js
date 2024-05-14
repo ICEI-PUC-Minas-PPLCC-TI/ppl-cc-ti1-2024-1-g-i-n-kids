3ef8e95a350754696d4a5149f36639bbeadf586d
Julia Cronenberger <jcronenberger@sga.pucminas.br>
2024-05-14 12:03:15 -0300

    feat - adição da funcionalidade de cadastrar usuários

--- codigo/assets/js/usuario/cadastro.js
+++ codigo/assets/js/usuario/cadastro.js
@@ -1,1 +1,35 @@
-console.log('JavaScript da página de cadastro');
+function init() {
+    let formularioCadastro = document.querySelector('form');
+    let botaoCadastro = document.getElementById('botao-cadastro');
+
+    botaoCadastro.addEventListener('click', (e) => {
+        let campoNome = document.getElementById('nome').value;
+        let campoTelefone =  document.getElementById('telefone').value;
+        let campoEmail = document.getElementById('email').value;
+        let campoPassword =  document.getElementById('password').value;
+        let campoConfirmPassword =  document.getElementById('confirm-password').value;
+
+        e.preventDefault();
+
+        if (!formularioCadastro.checkValidity()) {
+            displayMessage('Preencha o formulário corretamente.', 'warning');
+            return;
+        }
+
+        if (campoPassword !== campoConfirmPassword) {
+            displayMessage('As senhas não se coincidem.', 'warning');
+            return;
+        }
+        
+        let usuario = {
+            nome: campoNome,
+            telefone: campoTelefone,
+            email: campoEmail,
+            senha: campoPassword
+        };
+
+        createUser(usuario);
+
+        formularioCadastro.reset();
+    });
+}
