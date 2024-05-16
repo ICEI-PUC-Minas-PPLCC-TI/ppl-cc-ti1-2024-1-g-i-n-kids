7af4a92d9b67f7dfa82309560341668feeac3508
Lucas Berao <lalvesberao@gmail.com>
2024-05-16 20:04:54 -0300

    feat: Json feito

--- codigo/assets/js/contato/contato.js
+++ codigo/assets/js/contato/contato.js
@@ -1,1 +1,26 @@
-console.log('JavaScript da página de contato');
+function init(){
+    let formularioContatos = document.querySelector('form');
+    let btnCadastrarContatos = document.getElementById('btnCadastrarContato');
+
+    btnCadastrarContatos.addEventListener('click',()=>{
+        let campoNome = document.getElementById('nome').value;
+        let campoEmail = document.getElementById('email').value;
+        let campoMensagem = document.getElementById('mensagem').value;
+
+        if (!formularioContatos.checkValidity()) {
+            displayMessage('Preencha o formulário corretamente.', 'warning');
+            return;
+        }
+
+        let contatos = {
+            nome_usuario: campoNome,
+            email: campoEmail,
+            mensagem: campoMensagem,
+        };
+
+        createContact(contatos);
+
+        formularioContatos.reset();
+
+    });
+}
