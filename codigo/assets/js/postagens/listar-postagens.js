076881d607e8f604079cbbb81f402b0b3fbaa063
Davi Lanna <davi.ayreslanna@gmail.com>
2024-05-17 14:55:27 -0300

    Ajustes no visual da pagina de postagens.

--- codigo/assets/js/postagens/listar-postagens.js
+++ codigo/assets/js/postagens/listar-postagens.js
@@ -15,10 +15,12 @@ function listPosts() {
 
             divpostagens.innerHTML += `
                                         <div class="card" style="width: 18rem;">
-                                          <h3 class="card-title">${post.titulo} 1</h3>
+                                          
                                           <img class="card-img-top" src="${post.link_imagem}" alt="Imagem da Postagem 1">
                                           <div class="card-body">
                                             <div class="descricao">
+                                              <h3 class="card-title">${post.titulo}</h3>
+                                              <h5 class="autor">${post.autor}
                                             </div>
                                           </div>
                                         </div>`;
